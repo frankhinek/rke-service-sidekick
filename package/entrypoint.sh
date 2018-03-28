06229c3a9815e548ecaec0249521bba255d7b0fe
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2018-03-28 03:36:38 +0200

    Add Azure cloud provider

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -x
 
+# generate Azure cloud provider config
+if echo ${@} | grep -q "cloud-provider=azure"; then
+  if [ "$1" == "kubelet" ] || [ "$1" == "kube-apiserver" ]; then
+    source /opt/rke/cloud-provider.sh
+    set_azure_config
+  fi
+fi
+
 if [ "$1" == "kubelet" ]; then
     for i in $(DOCKER_API_VERSION=1.24 ./docker info 2>&1  | grep -i 'docker root dir' | cut -f2 -d:) /var/lib/docker /run /var/run; do
         for m in $(tac /proc/mounts | awk '{print $2}' | grep ^${i}/); do
